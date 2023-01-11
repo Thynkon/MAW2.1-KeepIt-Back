5f71d8c77bfb94184991149775ae1cd6546a937e
SemicolonManifest <theo.gautier@cpnv.ch>
2023-01-11 16:09:20 +0100

    fix by_title issue

--- app/lib/the_movie_db_client.rb
+++ app/lib/the_movie_db_client.rb
@@ -1,4 +1,6 @@
-class TheMovieDbCLient < FilmClient
+require 'cgi'
+
+class TheMovieDbClient < FilmClient
   def by_id(id)
     url = Rails.configuration.film_api_url
     api_key = ENV['THE_MOVIE_DB_API_3_KEY']
@@ -16,13 +18,15 @@ class TheMovieDbCLient < FilmClient
     end
   end
 
-  def by_title(title)
+  def by_title(title,page=1)
     url = Rails.configuration.film_api_url
     api_key = ENV['THE_MOVIE_DB_API_3_KEY']
     language = Rails.configuration.language # Could be replaced by i18n locales but in config file for now
 
+    title = CGI.escape(title)
+
     response = HTTP.headers(:accept => "application/json")
-    .get("#{url}/3/search/movie?api_key=#{api_key}&language=#{language}&query=#{title}")
+    .get("#{url}/3/search/movie?api_key=#{api_key}&language=#{language}&query=#{title}&page=#{page}")
 
     if response.status.success?
       films = JSON.parse(response.body)
