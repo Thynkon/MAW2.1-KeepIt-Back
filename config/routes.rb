33ddd08eb3a91e48b22da8b027246957281eea55
Anthony <bouillant.anthony@gmail.com>
2023-01-12 23:57:08 +0100

    Update route
    Add users and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'books', to: 'books#index'
   get 'books/search', to: 'books#search'
+
+  resources :users
+  post 'auth/login', to: 'authentication#authenticate'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
