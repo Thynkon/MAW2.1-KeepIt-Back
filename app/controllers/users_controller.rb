b2543a0425513dcab0e6ca7c884d85f7c64d498c
Anthony <bouillant.anthony@gmail.com>
2023-01-12 23:55:11 +0100

    Create User controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+    skip_before_action :authenticate_request, only: [:create, :show, :index]
+    before_action :check_user, only: [ :update, :destroy]
+
+    # GET /users
+    def index
+        @users = User.all
+        render json: @users, status: :ok
+    end
+    
+    # GET /users/username
+    def show
+        @user = User.find(params[:id])
+        render json: @user, status: :ok
+    end
+
+    # POST /users
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    # PATCH/PUT /users/1
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            render json: @user, status: :ok
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE /users/1
+    def destroy
+        @user.destroy
+        head :no_content
+    end
+
+    private
+        def check_user
+            if @current_user != User.find(params[:id])
+                render json: { errors: 'You are not authorized to access this resource' }, status: :unauthorized
+            end
+        end
+
+        def user_params
+            params.permit(:username, :email, :firstname, :lastname, :password)
+        end
+end
