8fbb4f7d64308870768255c2ef4ea26d9330740e
thynkon <thynkon@protonmail.com>
2023-03-22 15:49:43 +0100

    Document users achievements routes using swagger

--- spec/requests/api/achievements_spec.rb
+++ spec/requests/api/achievements_spec.rb
@@ -29,4 +29,31 @@ RSpec.describe 'Books API', type: :request do
       end
     end
   end
+
+  # Achievements
+  path '/users/{user_id}/achievements' do
+    get 'Revieves users achievements' do
+      tags 'Achievements'
+      security [Bearer: {}]
+      parameter name: :user_id, in: :path, type: :integer
+      parameter name: :id, in: :path, type: :integer
+
+      response '200', 'book upvoted' do
+        schema '$ref' => '#/components/schemas/achievements'
+      end
+    end
+  end
+
+  path '/users/{user_id}/achievements/{id}' do
+    get 'Revieves users achievements' do
+      tags 'Achievements'
+      security [Bearer: {}]
+      parameter name: :user_id, in: :path, type: :integer
+      parameter name: :id, in: :path, type: :integer
+
+      response '200', 'book upvoted' do
+        schema '$ref' => '#/components/schemas/achievement'
+      end
+    end
+  end
 end
