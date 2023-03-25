9a2e141624a4dc609f354bc4be384048dc23ca62
Armand Marechal <sypenfu@gmail.com>
2023-03-25 19:16:50 +0100

    Fix returned format on user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,10 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
-          render json: @user, status: :created
+        @current_user = current_user
+        render :show
       else
-          render json: @user.errors, status: :unprocessable_entity
+        render json: @user.errors, status: :unprocessable_entity
       end
     end
 
@@ -32,7 +33,8 @@ class UsersController < ApplicationController
     def update
       @user = User.find(params[:id])
       if @user.update(user_params)
-          render json: @user, status: :ok
+        @current_user = current_user
+        render :show
       else
           render json: @user.errors, status: :unprocessable_entity
       end
